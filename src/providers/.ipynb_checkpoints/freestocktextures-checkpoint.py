abab08d3933ef501049a76b983766346a0e64976
escatay <lea.emilia.fried@googlemail.com>
2022-08-03 09:34:11 +0200

    added file with requirements + corrected a typo

---
+++ src/providers/.ipynb_checkpoints/freestocktextures-checkpoint.py
@@ -0,0 +1,78 @@
+from provider import Provider
+
+import requests
+import bs4
+import urllib.request
+import io
+
+from PIL import Image
+
+class FreeStockTextures(Provider):
+    '''
+    '''
+
+    def __init__(self):
+        pass
+
+    def get_image(self, prompt):
+        '''
+        Returns an image or a number of images based on a prompt
+        '''
+
+        # Get the image from the freestocktextures.com website
+        response = requests.get('https://www.freestocktextures.com/search/?q=' + prompt)
+        soup = bs4.BeautifulSoup(response.text, 'html.parser')
+
+        # get h1 tag text
+        h1 = soup.find('h1')
+        #make sure we found the page title
+        assert h1 != None
+
+        # check if anything was found
+        if (h1.text.split()[0] == "Sorry"):
+            return None
+
+        # find ul class list
+        texture_list = soup.find('ul', class_='list')
+
+        #find all img in that list
+        images = texture_list.find_all('img')
+        assert len(images) > 0 and len(images) <= 10
+
+        downloaded_images = []
+        for i in images:
+            #assert 'src' in i
+            print(i['src'])
+            img_dl = urllib.request.urlopen(i['src'])
+            img_file = io.BytesIO(img_dl.read())
+            #downloaded_images.append(img_file)
+
+            #crop square from middle of image
+            img = Image.open(img_file)
+            shorteredge = min(img.size)
+            w, h = img.size
+
+            left = (w - shorteredge) / 2
+            top = (h - shorteredge) / 2
+            right = (w + shorteredge) / 2
+            bottom = (h + shorteredge) / 2
+
+            img = img.crop((left, top, right, bottom))
+            downloaded_images.append(img)
+
+            img.show()
+
+
+
+
+        return downloaded_images
+
+if __name__ == '__main__':
+    prompt = input('Enter a prompt: ')
+    provider = FreeStockTextures()
+    res = provider.get_image(prompt)
+    #print(res)
+    
+    from PIL import Image
+    img = Image.open(res[0])
+    img.show()
