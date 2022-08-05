47d51581913eba9d040f7105518417f94e0827db
sva <theskalds@gmail.com>
2022-08-05 23:11:00 +0200

    maybe this time

--- src/text2nca/providers/dalle_mini.py
+++ src/text2nca/providers/dalle_mini.py
@@ -25,6 +25,27 @@ DALLE_COMMIT_ID = None
 VQGAN_REPO = "dalle-mini/vqgan_imagenet_f16_16384"
 VQGAN_COMMIT_ID = "e93a26e7707683d349bf5d5c41c5b0ef69b677a9"
 
+# compile and parallelize model function to use multiple devices
+# model inference
+@partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(4, 5, 6, 7))
+def p_generate(
+    tokenized_prompt, key, params, top_k, top_p, temperature, condition_scale
+):
+    return model.generate(
+        **tokenized_prompt,
+        prng_key=key,
+        params=params,
+        top_k=top_k,
+        top_p=top_p,
+        temperature=temperature,
+        condition_scale=condition_scale,
+    )
+
+
+# decode image
+@partial(jax.pmap, axis_name="batch")
+def p_decode(indices, params):
+    return vqgan.decode_code(indices, params=params)
 
 class DallEMini(Provider):
     '''
@@ -32,14 +53,17 @@ class DallEMini(Provider):
     '''
 
     def __init__(self):
-        #super().__init__()
+        global model
+        #global params
+        global vqgan
+        #global vqgan_params
         # Load dalle-mini
-        self.model, self.params = DalleBart.from_pretrained(
+        model, self.params = DalleBart.from_pretrained(
             DALLE_MODEL, revision=DALLE_COMMIT_ID, dtype=jnp.float16, _do_init=False
         )
 
         # Load VQGAN
-        self.vqgan, self.vqgan_params = VQModel.from_pretrained(
+        vqgan, self.vqgan_params = VQModel.from_pretrained(
             VQGAN_REPO, revision=VQGAN_COMMIT_ID, _do_init=False
         )
         
@@ -54,27 +78,6 @@ class DallEMini(Provider):
 
         self.processor = DalleBartProcessor.from_pretrained(DALLE_MODEL, revision=DALLE_COMMIT_ID)
 
-    # compile and parallelize model function to use multiple devices
-    # model inference
-    @partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(4, 5, 6, 7))
-    def p_generate(
-        self, tokenized_prompt, key, params, top_k, top_p, temperature, condition_scale
-    ):
-        return self.model.generate(
-            **tokenized_prompt,
-            prng_key=key,
-            params=params,
-            top_k=top_k,
-            top_p=top_p,
-            temperature=temperature,
-            condition_scale=condition_scale,
-        )
-
-
-    # decode image
-    @partial(jax.pmap, axis_name="batch")
-    def p_decode(self, indices, params):
-        return self.vqgan.decode_code(indices, params=params)
 
         
 
@@ -100,7 +103,7 @@ class DallEMini(Provider):
             # get a new key
             self.key, subkey = jax.random.split(self.key)
             # generate images
-            encoded_images = self.p_generate(
+            encoded_images = p_generate(
                 tokenized_prompt=tokenized_prompt,
                 key=shard_prng_key(subkey),
                 params=self.params,
@@ -112,7 +115,7 @@ class DallEMini(Provider):
             # remove BOS
             encoded_images = encoded_images.sequences[..., 1:]
             # decode images
-            decoded_images = self.p_decode(encoded_images, self.vqgan_params)
+            decoded_images = p_decode(encoded_images, self.vqgan_params)
             decoded_images = decoded_images.clip(0.0, 1.0).reshape((-1, 256, 256, 3))
             for decoded_img in decoded_images:
                 # TODO only append to images in last step
