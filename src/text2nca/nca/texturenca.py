f9f880ff63b27956fff4e1993cae18b3c5405126
sva <theskalds@gmail.com>
2022-08-05 23:42:41 +0200

    fix nca

--- src/text2nca/nca/texturenca.py
+++ src/text2nca/nca/texturenca.py
@@ -1,53 +1,30 @@
 from text2nca.nca.nca import NCA
 
 import os
-import io
-import PIL.Image, PIL.ImageDraw, PIL.ImageFont 
-import base64
-import zipfile
-import json
-import requests
-import random
 import numpy as np
 import matplotlib.pylab as pl
-import glob
-import ipywidgets as widgets
-from concurrent.futures import ThreadPoolExecutor
-from ipywidgets import Layout
-from string import Template
 
-import tensorflow as tf
+from IPython.display import Image, clear_output
 
-from IPython.display import Image, HTML, clear_output, Javascript
-from tqdm import tqdm_notebook
-
-os.environ['FFMPEG_BINARY'] = 'ffmpeg'
-import moviepy.editor as mvp
-from moviepy.video.io.ffmpeg_writer import FFMPEG_VideoWriter
-
-from ml_collections import ConfigDict
-
-from self_organising_systems import texture_ca
-from self_organising_systems.texture_ca.losses import StyleModel, Inception
+from self_organising_systems.texture_ca.losses import StyleModel
 from self_organising_systems.texture_ca.texture_synth import TextureSynthTrainer
-from self_organising_systems.texture_ca import ca
 from self_organising_systems.texture_ca.ca import to_rgb
 from self_organising_systems.texture_ca.config import cfg
-from self_organising_systems.shared.util import imread, imencode, imwrite, zoom, tile2d
-from self_organising_systems.texture_ca.export_models import export_models_to_js 
+from self_organising_systems.shared.util import imencode, imwrite
 
 def imshow(a, fmt='jpeg'):
   display(Image(data=imencode(a, fmt)))
 
 class TextureNCA(NCA):
     def __init__(self):
-        self.trainer = TextureSynthTrainer(loss_model = self.loss_model)
+        pass
 
     def train(self, name, image):
         self.image = np.float32(image)/255.0
         self.name = name
         imwrite('_target.png', self.image)
         self.loss_model = StyleModel('_target.png')
+        self.trainer = TextureSynthTrainer(loss_model = self.loss_model)
 
         imshow(self.image)
 
