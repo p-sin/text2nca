23962b7e355845c06aa7539bd128dcaebfd76ae3
sva <theskalds@gmail.com>
2022-08-05 21:46:30 +0200

    nca fixes

--- src/text2nca/nca/texturenca.py
+++ src/text2nca/nca/texturenca.py
@@ -36,14 +36,36 @@ from self_organising_systems.texture_ca.config import cfg
 from self_organising_systems.shared.util import imread, imencode, imwrite, zoom, tile2d
 from self_organising_systems.texture_ca.export_models import export_models_to_js 
 
+def imshow(a, fmt='jpeg'):
+  display(Image(data=imencode(a, fmt)))
+
 class TextureNCA(NCA):
     def __init__(self, name, image):
         self.image = np.float32(image)/255.0
-        self.filename = os.path.join(name + '.npy')
         imwrite('_target.png', self.image)
         self.loss_model = StyleModel('_target.png')
         self.trainer = TextureSynthTrainer(loss_model = self.loss_model)
+        self.name = name
 
-    def train_step(self):
-        r = self.trainer.train_step()
-        
+    def train(self):
+        try:
+          for i in range(cfg.texture_ca.train_steps):
+            # r is  Bunch(batch=batch, x0=x0, loss=loss, step_num=step_num)
+            r = self.trainer.train_step()
+            if i%10 == 0:
+              clear_output(True)
+              pl.yscale('log')
+              pl.plot(self.trainer.loss_log, '.', alpha=0.3)
+              pl.show()
+              vis = np.hstack(to_rgb(r.batch.x))
+              imshow(vis)
+              print('\r', len(self.trainer.loss_log), r.loss.numpy(), end='')
+            if (i+1)%500 == 0:
+              self.trainer.ca.save_params(self.name)
+        except KeyboardInterrupt:
+          pass
+        finally:
+          self.trainer.ca.save_params(self.name)
+          print('\nsaved model as "%s"'%self.name)
+
+            
